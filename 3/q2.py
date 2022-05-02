3d1e5583857471ed6be2db0482f198e9c723797b
yinjiahuidada <yinjiahuidada@gmail.com>
2022-05-02 15:47:23 +0800

    Update q2.py

--- 3/q2.py
+++ 3/q2.py
@@ -37,34 +37,98 @@ if __name__ == '__main__':
     cpx = cplex.Cplex()
 
     requirements = {
-        'May': {1: 22000, 2: 22000, 3: 25000, 4: 30000},
+        'May': {0: 0, 1: 22000, 2: 22000, 3: 25000, 4: 30000},
         'June': {1: 35000, 2: 35000, 3: 40000, 4: 40000},
         'July': {1: 45000, 2: 45000, 3: 45000, 4: 45000},
         'Aug': {1: 45000, 2: 45000, 3: 45000, 4: 45000},
         'Sep': {1: 12000, 2: 10000, 3: 10000, 4: 8000},
     }
     labour_apply = {
-        'May': {1: 800, 2: 200, 3: 200, 4: 200},
-        'June': {1: 200, 2: 200, 3: 200, 4: 200},
-        'July': {1: 100, 2: 100, 3: 100, 4: 100},
-        'Aug': {1: 100, 2: 100, 3: 100, 4: 100},
-        'Sep': {1: 0, 2: 0, 3: 0, 4: 0},
-    }
-    labour_leave = {
-        'May': {1: 800, 2: 200, 3: 200, 4: 200},
+        'May': {0: 700, 1: 800, 2: 200, 3: 200, 4: 200},
         'June': {1: 200, 2: 200, 3: 200, 4: 200},
         'July': {1: 100, 2: 100, 3: 100, 4: 100},
         'Aug': {1: 100, 2: 100, 3: 100, 4: 100},
         'Sep': {1: 0, 2: 0, 3: 0, 4: 0},
     }
     labour_usage = {'new': 10, 'exp': 30, }
-
+    exp_left = {
+        'May': {0: 1, 1: 0.85, 2: 0.85, 3: 0.85, 4: 0.85},
+        'June': {1: 0.85, 2: 0.85, 3: 0.85, 4: 0.85},
+        'July': {1: 0.85, 2: 0.85, 3: 0.85, 4: 0.85},
+        'Aug': {1: 0.85, 2: 0.85, 3: 0.85, 4: 0.25},
+        'Sep': {1: 0.9, 2: 0.9, 3: 0.9, 4: 0.9},
+    }
     var_names = [
         f'{requirement}_{i}'
         for requirement in requirements for i in requirements[requirement]
     ]
-    print(var_names)
-    lbs = np.hstack((np.array(800), np.zeros(len(var_names)-1)))  # 下界
-    ubs = [labour_apply[var_name.split('_')[0]][int(var_name.split('_')[-1])] for var_name in var_names]  # 上界
+
+    lbs = np.hstack((np.array(700), np.array(800), np.zeros(len(var_names) - 2)))  # 下界
+    ubs = [float(labour_apply[var_name.split('_')[0]][int(var_name.split('_')[-1])]) for var_name in var_names]  # 上界
     var_types = 'I' * len(var_names)  # 数据类型
-    print(var_types)
+
+    objective = [var_name for var_name in var_names]  # 总雇佣人数
+
+    # 约束条件
+    constraints_lefts = []  # 约束条件左边
+    constraints_rights = []  # 约束条件右边
+
+    # 工时满足
+    n = 1
+    _constraint = []
+    __constraint = [30/0.85]
+    for i, var_name in enumerate(var_names):
+        _constraint.append(var_name)
+        if i == 0:
+            # constraint = [_constraint.copy(), __constraint.copy()]
+            # constraints_lefts.append(constraint)
+            # constraints_rights.append(
+            #     requirements[var_name.split('_')[0]][int(var_name.split('_')[-1])]
+            # )
+            pass
+        else:
+            __constraint = [exp_left[var_name.split('_')[0]][int(var_name.split('_')[-1])] * x for x in __constraint]
+
+            # 新员工会在下一周成为有经验的员工
+            __constraint = __constraint[:-1]
+            __constraint += [labour_usage['exp']]
+
+            # 新员工
+            __constraint += [labour_usage['new']]
+
+            # 约束条件
+            constraint = [_constraint.copy(), __constraint.copy()]
+            constraints_lefts.append(constraint)
+            constraints_rights.append(
+                requirements[var_name.split('_')[0]][int(var_name.split('_')[-1])]
+            )
+
+    constraints_senses = 'G' * len(constraints_rights)
+    constraints_names = [f'c{i}' for i in range(len(constraints_lefts))]  # 约束规则名
+
+    try:
+        cpx.objective.set_sense(cpx.objective.sense.minimize)  # 求解目标: 最小值
+        cpx.variables.add(
+            obj=objective,
+            lb=lbs,
+            ub=ubs,
+            types=var_types,
+            names=var_names
+        )  # 设置变量
+        cpx.linear_constraints.add(
+            lin_expr=constraints_lefts,
+            senses=constraints_senses,
+            rhs=constraints_rights,
+            names=constraints_names
+        )  # 添加约束
+
+        cpx.solve()  # 问题求解
+        x = cpx.solution.get_values()  # 最优情况下的变量值
+        objective_value = cpx.solution.get_objective_value()  # 最优情况下的目标值
+
+        for i, var_name in enumerate(var_names):
+            LOGGER.info(f'最优自变量 {var_name}: {x[i]}')
+        LOGGER.info(f'最优结果: {objective_value}')
+
+    except CplexError as e:
+        LOGGER.error(e)
